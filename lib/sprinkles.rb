f3c8c7adda74348af535386e151665ff597801f3
Craig R Webster <craig@barkingiguana.com>
2009-03-13 11:54:25 +0000

    Pull in all dependencies in the main include file.

--- lib/sprinkles.rb
+++ lib/sprinkles.rb
@@ -1,4 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
 require 'sprinkles/processor/ping'
+require 'sprinkles/processor/logger'
+require 'sprinkles/processor/greeter'
+require 'sprinkles/origin'
 require 'sprinkles/bot'
